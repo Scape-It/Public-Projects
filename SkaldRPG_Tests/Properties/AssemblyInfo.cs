4c3cc8942f6cb43d831f8a5a1ba28104bdc21da8
Olivier Guinart <olivier_guinart+github@live.com>
2020-01-16 12:08:19 -0800

    Adding unit tests project

---
+++ SkaldRPG_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SkaldRPG_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SkaldRPG_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3dd44720-8d52-4fd4-b8e5-74e3ac3d5d9d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SkaldRPG_Tests/VehicleContainerTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkaldRPG;
+using static SkaldRPG.VehicleContainer;
+
+namespace SkaldRPG_Test
+{
+    [TestClass]
+    public class VehicleContainerTests
+    {
+        private VehicleContainer vehicleContainer;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            vehicleContainer = new VehicleContainer();
+        }
+
+        [TestMethod]
+        public void MakeShip_Bogus_Non_Chartered_Test()
+        {
+            // Arrange and act
+            vehicleContainer.MakeShip("Bogus");
+            Vehicle vehicle = vehicleContainer.GetVehicle();
+
+            // Assert
+            Assert.IsTrue(vehicle.IsChartered == false);
+            Assert.AreEqual("A ship lies at anchor.\n\nYou may charter this ship if you can afford it", vehicle.getDescription());
+        }
+
+        [TestMethod]
+        public void MakeShip_Bogus_Chartered_Test()
+        {
+            // Arrange and act
+            vehicleContainer.MakeShip("Bogus");
+            Vehicle vehicle = vehicleContainer.GetVehicle();
+            vehicle.CharterVehicle();
+
+            // Assert
+            Assert.IsTrue(vehicle.IsChartered == true);
+            Assert.AreEqual("A ship lies at anchor.\n\nThis ship has been chartered and you may come onboard!", vehicle.getDescription());
+        }
+
+        [TestMethod]
+        public void MakeShip_Kogge_LowerCase_Uppercase_Test()
+        {
+            // Arrange and act
+            vehicleContainer.MakeShip("Kogge");
+            Vehicle kogge_lowerCase = vehicleContainer.GetVehicle();
+
+            vehicleContainer.MakeShip("KOGGE");
+            Vehicle kogge_upperCase = vehicleContainer.GetVehicle();
+
+            // Assert
+            Assert.AreEqual("Models/Kogge", kogge_lowerCase.ModelPath);
+            Assert.AreEqual(kogge_lowerCase.ModelPath, kogge_upperCase.ModelPath);
+        }
+
+        [TestMethod]
+        public void SetVehicle_Test()
+        {
+            // Arrange and act
+            vehicleContainer.MakeShip("Kogge");
+            vehicleContainer.SetVehicle(vehicleContainer);
+
+            // Assert
+            Assert.IsNotNull(vehicleContainer.GetVehicle());
+        }
+    }
+}
